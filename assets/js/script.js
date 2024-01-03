14a061dd485aff5297ecf835d20f75cf69560dc1
mundiabrian <mundiabrian687@gmail.com>
2024-01-03 13:42:16 +0300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+/**
+ * show go top btn when scroll window to 500px
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 500 ? goTopBtn.classList.add("active")
+    : goTopBtn.classList.remove("active");
+});
